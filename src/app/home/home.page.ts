296f35b1fa9ac918059b418661a02aa000d2a375
dubalumesh <dubalumesh@gmail.com>
2020-04-28 12:35:53 +0530

    first Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Capacitor, Plugins, CameraResultType, FilesystemDirectory } from '@capacitor/core';
+const { Camera, Filesystem } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,33 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  async takePhoto() {
+    const options = {
+      resultType: CameraResultType.Uri
+    };
+
+    const originalPhoto = await Camera.getPhoto(options);
+    debugger
+    const photoInTempStorage = await Filesystem.readFile({ path: originalPhoto.path });
+debugger;
+    let date = new Date(),
+      time = date.getTime(),
+      fileName = time + ".jpeg";
+
+    await Filesystem.writeFile({
+      data: photoInTempStorage.data,
+      path: fileName,
+      directory: FilesystemDirectory.Data
+    });
+
+    const finalPhotoUri = await Filesystem.getUri({
+      directory: FilesystemDirectory.Data,
+      path: fileName
+    });
+
+    let photoPath = Capacitor.convertFileSrc(finalPhotoUri.uri);
+    console.log(photoPath);
+  }
 
 }
